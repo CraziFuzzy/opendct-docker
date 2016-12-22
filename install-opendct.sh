8e11bc4d9f666099806fcd3efc3eb713d8bc051c
CraziFuzzy <fuzzy@weirdness.com>
2016-12-21 17:30:59 -0800

    Switch to supervisord, update to 5.17

--- install-opendct.sh
+++ install-opendct.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install opendct
-wget https://dl.bintray.com/opendct/Beta/releases/0.5.12/opendct_0.5.12-1_amd64.deb
-dpkg -i opendct_0.5.12-1_amd64.deb
-rm opendct_0.5.12-1_amd64.deb
+wget https://dl.bintray.com/opendct/Beta/releases/0.5.17/opendct_0.5.17-1_amd64.deb
+dpkg -i opendct_0.5.17-1_amd64.deb
+rm opendct_0.5.17-1_amd64.deb
 echo "OpenDCT Install Complete"
