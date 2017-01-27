59f83e2aaf929f8e75bbf9fadf8fa92bbc170b0a
Christopher Piper <CraziFuzzy@users.noreply.github.com>
2017-01-27 12:44:16 -0800

    Enable version forcing.

--- install-opendct.sh
+++ install-opendct.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+VERSION=${VERSION:-latest}
+
+# OpenDCT Version
+OPENDCT_VERSION=""
+if [ "${VERSION}" = "latest" ] ; then
+    OPENDCT_VERSION=`curl -L https://bintray.com/opendct/Beta/OpenDCT/_latestVersion | egrep -o '\/opendct\/Beta\/OpenDCT\/[0-9]*\.[0-9]*\.[0-9]*' | tail -1 | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+else
+    OPENDCT_VERSION=${VERSION}
+fi
+
 # install opendct
-OPENDCT_VERSION=`curl -L https://bintray.com/opendct/Beta/OpenDCT/_latestVersion | egrep -o '\/opendct\/Beta\/OpenDCT\/[0-9]*\.[0-9]*\.[0-9]*' | tail -1 | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
 OPENDCT_URL=https://dl.bintray.com/opendct/Beta/releases/${OPENDCT_VERSION}/opendct_${OPENDCT_VERSION}-1_amd64.deb
 OPENDCT_DEB=opendct_${OPENDCT_VERSION}-1_amd64.deb
 
