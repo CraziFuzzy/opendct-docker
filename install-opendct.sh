aa7a7475a8963e863c287bfaabb977132d504674
Christopher Piper <CraziFuzzy@users.noreply.github.com>
2017-01-27 14:05:28 -0800

    Fixed stable download URL.

--- install-opendct.sh
+++ install-opendct.sh
@@ -6,16 +6,19 @@ VERSION=${VERSION:-latest}
 OPENDCT_VERSION=""
 if [ "${VERSION}" = "latest" ] ; then
     OPENDCT_VERSION=`curl -L https://bintray.com/opendct/Releases/OpenDCT/_latestVersion | egrep -o '\/opendct\/Releases\/OpenDCT\/[0-9]*\.[0-9]*\.[0-9]*' | tail -1 | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+    OPENDCT_URL=https://dl.bintray.com/opendct/Releases/releases/${OPENDCT_VERSION}/opendct_${OPENDCT_VERSION}-1_amd64.deb
+    OPENDCT_DEB=opendct_${OPENDCT_VERSION}-1_amd64.deb
 elif [ "${VERSION}" = "beta" ] ; then
     OPENDCT_VERSION=`curl -L https://bintray.com/opendct/Beta/OpenDCT/_latestVersion | egrep -o '\/opendct\/Beta\/OpenDCT\/[0-9]*\.[0-9]*\.[0-9]*' | tail -1 | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+    OPENDCT_URL=https://dl.bintray.com/opendct/Beta/releases/${OPENDCT_VERSION}/opendct_${OPENDCT_VERSION}-1_amd64.deb
+    OPENDCT_DEB=opendct_${OPENDCT_VERSION}-1_amd64.deb
 else
     OPENDCT_VERSION=${VERSION}
+    OPENDCT_URL=https://dl.bintray.com/opendct/Beta/releases/${OPENDCT_VERSION}/opendct_${OPENDCT_VERSION}-1_amd64.deb
+    OPENDCT_DEB=opendct_${OPENDCT_VERSION}-1_amd64.deb
 fi
 
 # install opendct
-OPENDCT_URL=https://dl.bintray.com/opendct/Beta/releases/${OPENDCT_VERSION}/opendct_${OPENDCT_VERSION}-1_amd64.deb
-OPENDCT_DEB=opendct_${OPENDCT_VERSION}-1_amd64.deb
-
 echo "Installing OpenDCT ${OPENDCT_VERSION}..."
 
 wget -O ${OPENDCT_DEB} ${OPENDCT_URL}
